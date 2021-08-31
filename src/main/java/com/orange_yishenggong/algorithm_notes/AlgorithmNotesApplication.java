8cea85a234d4d3ef73b2c0b40402c4ae2f820e10
gongyisheng <Orangepass!2019>
2021-08-31 10:04:50 -0500

    0.1 test

---
+++ src/main/java/com/orange_yishenggong/algorithm_notes/AlgorithmNotesApplication.java
@@ -0,0 +1,13 @@
+package com.orange_yishenggong.algorithm_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmNotesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/orange_yishenggong/algorithm_notes/graph/TopologicalSort.java
@@ -0,0 +1,5 @@
+package com.orange_yishenggong.algorithm_notes.graph;
+
+public class TopologicalSort {
+
+}
