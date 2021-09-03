722ebbfaf58ccbf380c5c00edd1d8aa5bebc5d6c
gongyisheng <Orangepass!2019>
2021-09-02 22:41:47 -0500

    0.10 add Hierholzer's Algorithm

--- src/main/java/com/orange_yishenggong/algorithm_notes/AlgorithmNotesApplication.java
+++ src/main/java/com/orange_yishenggong/algorithm_notes/AlgorithmNotesApplication.java
@@ -1,11 +1,7 @@
 package com.orange_yishenggong.algorithm_notes;
 
-import com.orange_yishenggong.algorithm_notes.graph.Kruskal;
-import com.orange_yishenggong.algorithm_notes.graph.Prim;
-import com.orange_yishenggong.algorithm_notes.graph.TopologicalSort;
+import com.orange_yishenggong.algorithm_notes.graph.*;
 import com.orange_yishenggong.algorithm_notes.tests.ts;
-import com.orange_yishenggong.algorithm_notes.graph.UnionFind;
-import com.orange_yishenggong.algorithm_notes.graph.DFS;
 import com.orange_yishenggong.algorithm_notes.tests.uf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +11,7 @@ public class AlgorithmNotesApplication {
     private static int[][] graph1 = new int[][]{{1,0},{1,2},{2,3},{3,4},{1,4},{5,2},{2,6}};//has circle;
     private static int[][] graph2 = new int[][]{{1,2},{2,3},{3,4},{1,4},{1,5}};
     private static int[][] points1 = new int[][]{{3,12},{-2,5},{-4,1}};
+    private static String[][] tickets = new String[][]{{"JFK","KUL"},{"JFK","NRT"},{"NRT","JFK"}};
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmNotesApplication.class, args);
         System.out.println(TopologicalSort.hasCircle(graph1));
@@ -23,5 +20,6 @@ public class AlgorithmNotesApplication {
         System.out.println(Kruskal.minCostWithPoints(points1));
         System.out.println(Prim.minCostWithPoints(points1));
         System.out.println(DFS.findPath(7,graph1,1,6));
+        System.out.println(Hierholzer.findPath(tickets,"JFK"));
     }
 }
