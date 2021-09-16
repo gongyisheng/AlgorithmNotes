8bd6169bb74d518482372d932513974f98d98ac5
gongyisheng <Orangepass!2019>
2021-09-15 21:33:19 -0500

    0.21 update depth: add 2 methods for calc max depth

--- src/main/java/com/orange_yishenggong/algorithm_notes/binaryTree/Depth.java
+++ src/main/java/com/orange_yishenggong/algorithm_notes/binaryTree/Depth.java
@@ -5,19 +5,27 @@ import java.util.Queue;
 
 public class Depth {
     //Depth related problems
-    public int maxDepth = 0;
+    public int maxDepth = Integer.MIN_VALUE;
+    public int minDepth = Integer.MAX_VALUE;
     public void clearDepth(){
-        maxDepth = 0;
+        maxDepth = Integer.MIN_VALUE;
+        minDepth = Integer.MAX_VALUE;
     }
 
-    public void dfs(Node root,int depth){
+    //max depth
+    public int calcMaxDepth_DFS_int(Node root){
+        if(root==null) return 0;
+        return Math.max(calcMaxDepth_DFS_int(root.left),calcMaxDepth_DFS_int(root.right))+1;
+    }
+
+    public void calcMaxDepth_DFS_void(Node root,int depth){
         if(root==null) return;
         maxDepth = Math.max(maxDepth,depth);
-        dfs(root.left,depth+1);
-        dfs(root.right,depth+1);
-
+        calcMaxDepth_DFS_void(root.left,depth+1);
+        calcMaxDepth_DFS_void(root.right,depth+1);
     }
-    public void bfs(Node root){
+
+    public void calcMaxDepth_BFS(Node root){
         Queue<Node> q = new LinkedList<>();
         q.offer(root);
         maxDepth = 0;
@@ -27,6 +35,47 @@ public class Depth {
             maxDepth++;
             for(int i=0;i<size;i++){
                 Node node = q.poll();
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+        }
+    }
+    
+    //min depth
+    public int calcMinDepth_DFS_int(Node root){
+        if(root == null) return 0;
+
+        if(root.left == null) return calcMinDepth_DFS_int(root.right) + 1;
+        if(root.right == null) return calcMinDepth_DFS_int(root.left) + 1;
+
+        return Math.min(calcMinDepth_DFS_int(root.left), calcMinDepth_DFS_int(root.right)) + 1;
+    }
+
+    public void calcMinDepth_DFS_void(Node root,int depth){
+        if(root==null) return;
+        if(root.left==null&&root.right==null){
+            minDepth = Math.min(minDepth,depth);
+        }
+        calcMinDepth_DFS_void(root.left,depth+1);
+        calcMinDepth_DFS_void(root.right,depth+1);
+    }
+
+    public void calcMinDepth_BFS(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int depth = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            depth++;
+            for(int i=0;i<size;i++){
+                Node node = q.poll();
+                if(node.left==null&&node.right==null) {
+                    minDepth = depth;
+                    return;
+                }
                 if (node.left != null)
                     q.offer(node.left);
                 if (node.right != null)
