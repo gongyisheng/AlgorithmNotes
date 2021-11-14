eaa23e671ac01aaa0ab23606ddb3d00414cdebb3
gongyisheng <Orangepass!2019>
2021-11-13 22:23:00 -0600

    0.30 add listnode

---
+++ src/main/java/com/orange_yishenggong/algorithm_notes/nodeList/util/ListNode.java
@@ -0,0 +1,18 @@
+package com.orange_yishenggong.algorithm_notes.nodeList.util;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
