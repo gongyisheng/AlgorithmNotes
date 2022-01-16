66bd16619772a6ecb4fc10bdedfad6994494e197
gongyisheng <Orangepass!2019>
2022-01-16 15:57:28 -0600

    0.39 add bit manipulation

---
+++ src/main/java/com/orange_yishenggong/algorithm_notes/math/BitManipulation.java
@@ -0,0 +1,31 @@
+package com.orange_yishenggong.algorithm_notes.math;
+
+public class BitManipulation {
+    //Find the right most one in an number
+    public int findRightOne(int num){
+        return num&((~num)+1);
+    }
+    //Find out the only element with even count in the array
+    public int findOneEvenCountElement(int[] arr){
+        int eor = 0;
+        for(int i=0;i<arr.length;i++){
+            eor ^= arr[i];
+        }
+        return eor;
+    }
+    //Find out the only two elements with even count in the array
+    public int[] findTwoEvenCountElement(int[] arr){
+        int eor = 0;
+        for(int i=0;i<arr.length;i++){
+            eor ^= arr[i];
+        }
+        int leftOne = findRightOne(eor);
+        int n1 = 0;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i]&leftOne)!=0){
+                n1 ^= arr[i];
+            }
+        }
+        return new int[]{n1,eor^n1};
+    }
+}
