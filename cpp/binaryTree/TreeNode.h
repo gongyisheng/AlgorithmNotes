dd42544970343df8ac3db10817a1a224a93b373f
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-20 00:50:55 -0500

    add treenode header file

---
+++ cpp/binaryTree/TreeNode.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
